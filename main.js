2a9d1bd71865c135ab1d5f61193be89dcf9a8151
Chingiz Shigayev <shigayevphone@gmail.com>
2020-03-27 17:48:45 +0400

    db added

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 
-function startOnButtonClick() {
-drawTable();
+// class Application {
+//   startOnButtonClick() {
+//     drawTable;
+//   }
+// }
+
+function drawTable() {
+  const outputArray = [];
+  outputArray[0] = `<tr><th>Название банка</th><th>Вклад</th><th>Процент</th><th>Итоговая сумма</th></tr>`;
+
+  //   for (let i=0; i<this.stockArray.length; i++) {
+  //     const bankName = this.stockArray[i].name ;
+  //     const depositType = this.stockArray[i].ticker ;
+  //     const percent = this.stockArray[i]._yield ;
+  //     const price = this.stockArray[i].price ;
+  //     const amount = this.stockArray[i].amount ;
+  //     const total = this.stockArray[i].total ;
+  //     arr[i+1] = `<tr><td>${bankName}</td><td>${depositType}</td><td>${percent}</td><td>${finalAmount}</td></tr>`
+  //     arr[i+1] = this.getRowCode(name, ticker, _yield, price, amount, total, 'tr' + i) ;
+  // }
+
+  // outputArray[i] = `<tr><td>${result[i].bankName}</td><td>${result[i].depositType}</td><td>${result[i].percent}</td><td>${result[i].finalAmount}</td></tr>`;
+  containerHTML.innerHTML = `<table>${outputArray.join('')}</table>`;
 }
 
-  function drawTable() {
-    const arr = [] ;
-    arr[0] = '<tr><th>Название банка</th><th>Вклад</th><th>Процент</th><th>Итоговая сумма</th></tr>';
+const startAmountHTML = document.getElementById('input-start-amount');
+const additionalHTML = document.getElementById('input-additional');
+const monthsHTML = document.getElementById('input-months');
+const currencyHTML = document.getElementById('currency');
 
-        container.innerHTML = '<table>' + arr + '</table>';
-    }
+const buttonHTML = document.getElementById('start-calculate-button');
 
-const startAmountHTML = document.getElementById("input-start-amount");
-const additionalHTML = document.getElementById("input-additional");
-const monthsHTML = document.getElementById("input-months");
-const currencyHTML = document.getSelection
+const containerHTML = document.getElementById('output-container');
 
-const errorStart = document.getElementById("error-notification-start");
-const errorAdditional = document.getElementById("error-notification-additional");
-const errorPercent = document.getElementById("error-notification-percent");
-const errorDays = document.getElementById("error-notification-days");
-
-const buttonHTML = document.getElementById("start-calculate-button");
-
-const container = document.getElementById('output-container');
-
-buttonHTML.addEventListener("click", startOnButtonClick);
+buttonHTML.addEventListener("click", drawTable);
